70a94da4447b3297d3d27a4def91fd24f7802f38
Dabronzo <ayrtonterceiro@gmail.com>
2022-01-04 22:20:49 +0000

    Added more content to the json varible and refactored most of the functions

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 import json
 
+# Global Varibles
+visited_tabern = False
+visited_inn = False
+change_inn = False
+
 
 class Entity:
     """
@@ -8,38 +13,43 @@ class Entity:
     def __init__(self, name, health):
         self.name = name
         self.health = health
-    
+
     def take_hit(self, damage):
         self.damage = damage
         self.health -= damage
         print(f"The {self.name} got {self.damage} of damage")
         print(f"The {self.name} health now is {self.health}")
 
+
 class Enemy(Entity):
     """
     Subclass of Entity for all the enemies of the game
     """
     weapons = {}
+
     def __init__(self, type, weapon, damage):
         self.type = type
         if self.type == 'goblin':
             Entity.__init__(self, 'goblin', 40)
         elif self.type == 'troll':
             Entity.__init__(self, 'troll', 100)
-        self.weapons[weapon] = damage 
+        self.weapons[weapon] = damage
+
     def get_enemy_damage(self):
         return self.weapons.get('sword')
+
     def __del__(self):
         print(f"The {self.name} is dead")
-     
+
+
 class Player(Entity):
     """
     Subclass of Entity for the player
     """
-    def __init__(self, inputName):
-        self.inputName = inputName
-        Entity.__init__(self, self.inputName, 100)
-    
+    def __init__(self, input_name):
+        self.input_name = input_name
+        Entity.__init__(self, self.input_name, 100)
+
     inventory = {}
 
     def add_to_inventory(self, item, damage):
@@ -54,14 +64,10 @@ class Player(Entity):
                 print("You don't have a sword in yout inventory!")
         else:
             print("Your inventory is empty!")
-    
+
     def get_status(self):
         print(f"Player: {self.name} total health: {self.health}\n")
 
-# Global Varibles
-visited_tabern = False
-visited_inn = False
-change_inn = False
 
 def validate_yes_no(input):
     """
@@ -73,18 +79,21 @@ def validate_yes_no(input):
         elif input == 'n' or input == 'N':
             return 'no'
         else:
-            raise ValueError("The input is not aceptable.. must be an 'N' or 'n'")        
+            raise ValueError("Invalid input.. must be an 'N' or 'n'")
     except ValueError:
         print(f"{input} is an invalid answer, try again")
+
 
 def the_cave_two():
     """
     Function for the second part of the cave
     """
     global change_inn
-    print('----------------------------------------------------------------')
-    print('You go deeper on the cave you hear more steps aproaching')
-    print('Another goblin appears to attack you!')
+    cave_two_data = data_extractor()
+    main_text = cave_two_data["cave_second"]
+    dash_line = cave_two_data["division_line"]
+    print(dash_line)
+    print(main_text)
     while True:
         action = input("Will you fight? type 'Y' or 'N\n")
         if validate_yes_no(action):
@@ -95,7 +104,7 @@ def the_cave_two():
             while True:
                 if fight(player, enemy01):
                     break
-            print('----------------------------------------------------------------')
+            print(dash_line)
             print('The fight is over')
             del enemy01
             player.get_status()
@@ -107,9 +116,11 @@ def the_cave_one():
     """
     Function for the first part of the cave
     """
-    print('----------------------------------------------------------------')
-    print("As you enter the cave you notice an creature aproaching")
-    print("It's a goblin, and it's going to attack you")
+    cave_one_data = data_extractor()
+    entrance = cave_one_data["cave_first"]
+    dash_line = cave_one_data["division_line"]
+    print(dash_line)
+    print(entrance)
     while True:
         action = input("Will you fight? type 'Y' or 'N\n")
         if validate_yes_no(action):
@@ -120,16 +131,16 @@ def the_cave_one():
             while True:
                 if fight(player, enemy01):
                     break
-            print('----------------------------------------------------------------')
+            print(dash_line)
             print('The fight is over')
             del enemy01
             player.get_status()
             the_cave_two()
         else:
-            print('----------------------------------------------------------------')
+            print(dash_line)
             print("You don't have a sword to fight!!")
             print("Going back to the main road")
-            print('----------------------------------------------------------------')
+            print(dash_line)
             main_road()
     else:
         print("you ran away")
@@ -139,23 +150,26 @@ def the_mountain():
     """
     Function to process the mountain stage of the game
     """
-    print('----------------------------------------------------------------')
-    print("You are now on the way to the mountain")
-    print("You see a bunch of dead bodies and a dreadfull feeling about this place")
+    mountain_data = data_extractor()
+    first_part = mountain_data["mountain_begin"]
+    middle_part = mountain_data["mountain_middle"]
+    dash_line = mountain_data["division_line"]
+    print(dash_line)
+    print("You are now on the way to the mountain.\n")
+    print(first_part)
     while True:
         action = input("Do you want to proceed? type 'Y' or 'N'\n")
         if validate_yes_no(action):
             break
     if validate_yes_no(action) == 'yes':
-        print('----------------------------------------------------------------')
-        print("As you walk you hear a grunt aproaching")
-        print("Before you can even think about it an enormous Troll aprear!")
-        print("You can not run away this time. The fight will begin...")
+        print(dash_line)
+        print(middle_part)
         final_fight()
     else:
-        print('----------------------------------------------------------------')
+        print(dash_line)
         print("You are going back to the main road")
         main_road()
+
 
 def final_fight():
     """
@@ -170,13 +184,13 @@ def final_fight():
                 break
         print("You have finished the game")
 
+
 def game_over():
     """
     Function to handle the game over
     """
     print('----------------------------------------------------------------')
     print("Sorry, you have lost the game...")
-
 
 
 def fight(player, enemy):
@@ -191,15 +205,23 @@ def fight(player, enemy):
             break
     return True
 
+
 def the_inn():
     """
     Functions for the events on the Inn scenario
     """
     global visited_inn
+    inn_data = data_extractor()
+    first_time = inn_data["first_inn"]
+    inn_keeper = inn_data["inn_keeper"]
+    second_time = inn_data["second_inn"]
+    third_time = inn_data["third_inn"]
+    dash_line = inn_data["division_line"]
+    print(dash_line)
+    print("You are now at the Inn...\n")
     if change_inn:
-        print('----------------------------------------------------------------')
-        print("At the Inn")
-        print("Hey, you look tired. I have a room avalible, do you want to take a rest?")
+        print(dash_line)
+        print(third_time)
         while True:
             action = input("Do you will take a rest? type 'Y' or 'N'\n")
             if validate_yes_no(action):
@@ -208,30 +230,22 @@ def the_inn():
             print("Your health is fully restored")
             player.health = 100
         else:
-            print('----------------------------------------------------------------')
+            print(dash_line)
             print("You'll be back on the town")
-            the_town()       
+            the_town()
     if visited_inn is False:
-        print('----------------------------------------------------------------')
-        print("At the Inn")
-        print("As you enter at the Inn you see a man at the reception, you aproach and he says:")
-        print("Inn Keeper: Hello traveler, I shall warn you that we don't have more accommodations")
-        print("Inn Keeper: Since that beast is at the mountain everybody who lives there flew away...")
-        print("...and now they are here waiting for someone to kill the beast")
-        print("You tell the man that you'll kill the beast")
-        print("He laughts and take something behind his table")
-        print("Inn Keeper:  Here you go, at leas take this sword with you...\n")
-        print('----------------------------------------------------------------')
+        print(dash_line)
+        print(first_time)
+        print(inn_keeper)
         player.add_to_inventory('sword', 30)
     elif visited_inn and not change_inn:
-        print('----------------------------------------------------------------')
-        print("At the Inn")
-        print("Inn Keeper: Hello again, lost something?\n")
-        print('----------------------------------------------------------------')
+        print(dash_line)
+        print(second_time)
+        print(dash_line)
     elif visited_inn and change_inn:
-        print('----------------------------------------------------------------')
-        print("Inn Keeper: Hey you look better. Good luck today..\n")
-        print('----------------------------------------------------------------')
+        print(dash_line)
+        print(third_time)
+        print(dash_line)
     while True:
         print("Seams that you have nothing else to do here...")
         action = input("To go back to the Town type 'Y\n")
@@ -247,15 +261,15 @@ def data_extractor():
     Function to open the data file json and return it
     """
     with open("data.json") as data:
-        jsonData = json.load(data)
+        json_data = json.load(data)
         data.close()
-        return jsonData
+        return json_data
+
 
 def the_tavern():
     """
     Function to handle the actions inside the tavern at the village
     """
-    print("You are entering the Tavern...")
     global visited_tabern
     tavern_data = data_extractor()
     line_dash = tavern_data['division_line']
@@ -263,6 +277,8 @@ def the_tavern():
     old_man = tavern_data['old_man']
     torch_text = tavern_data['torch']
     second_time = tavern_data['second_time_tavern']
+    print(line_dash)
+    print("You are entering the Tavern...\n")
     if visited_tabern is False:
         print(line_dash)
         print(first_sentences)
@@ -281,41 +297,52 @@ def the_tavern():
         if validate_yes_no(action):
             break
     the_town()
-   
+
 
 def the_town():
     """
     Functions that runs the town scenario
     """
-    print('----------------------------------------------------------------')
-    print("The town has a couple of buildings and a center, you only see two options to enter\n")
-    print("The Tavern --------- to enter type 'T'")
-    print("The Inn ------------ to enter type 'I'")
-    print("Go back to the main road --- type 'E'\n")
+    town_data = data_extractor()
+    dash_line = town_data["division_line"]
+    main_message = town_data["town_main"]
+    option_tavern = town_data["tavern_option"]
+    option_inn = town_data["inn_option"]
+    option_exit = town_data["exit_option"]
+    print("You are now at the Village")
+    print(dash_line)
+    print(main_message)
+    print(option_tavern)
+    print(option_inn)
+    print(option_exit)
 
     while True:
         action = input("Type here your next action: \n")
         if validate_action_town(action):
             break
     action_handler_town(validate_action_town(action))
-    
+
 
 def the_cave():
     """
     Function that runs the cave senario
     """
-    print('----------------------------------------------------------------')
-    print("You stop at the entrance. is dark and you can hear something further down on the cave")
-    action = input("Do you want to proceed? type 'Y' for yes and 'N' for no.\n")
+    cave_data = data_extractor()
+    dash_line = cave_data["division_line"]
+    cave_entrance = cave_data["cave_entrance"]
+    proceed = cave_data["proceed_yes_no"]
+    print(dash_line)
+    print(cave_entrance)
+    action = input(proceed)
     if action == 'Y' or action == 'y':
         if(player.inventory.get('torch')):
+            print(dash_line)
             print('you are entering the cave')
             the_cave_one()
         else:
-            print('----------------------------------------------------------------')
+            print(dash_line)
             print('you can not porceed without a torch')
             print('Going back to the main road...\n')
-            print('----------------------------------------------------------------')
             main_road()
     else:
         print('Going back to the main road...\n')
@@ -324,20 +351,28 @@ def the_cave():
 
 def main_road():
     """
-    When called bring the player to the main road where can be decided wher to go next
-    with the three options
+    When called bring the player to the main road where
+    can be decided where to go next with the three options
     """
-    print('----------------------------------------------------------------')
-    print("You are in a main road, you only see three possible ways to proceed. Where you would like to go?\n")
-    print("The Cave -------- to go there type 'C'")
-    print("The Village -------- to go there type 'V'")
-    print("The Mountain ---- to go there type 'M'")
+    road_data = data_extractor()
+    entrance = road_data["main_road"]
+    dash_line = road_data["division_line"]
+    option_cave = road_data["cave_option"]
+    option_village = road_data["village_option"]
+    option_mountain = road_data["mountain_option"]
+
+    print(dash_line)
+    print(entrance)
+    print(option_cave)
+    print(option_village)
+    print(option_mountain)
 
     while True:
         action = input("Type here your next action: \n")
         if validate_action_road(action):
             break
     action_handler_road(validate_action_road(action))
+
 
 def validate_action_town(action):
     """
@@ -355,6 +390,7 @@ def validate_action_town(action):
     except ValueError:
         print(f"{action} is not valid, please try it again...")
 
+
 def action_handler_town(action):
     """
     function that will validate and handle the user action in the Town
@@ -365,7 +401,7 @@ def action_handler_town(action):
         the_inn()
     elif action == 'exit':
         main_road()
-       
+
 
 def validate_action_road(action):
     """
@@ -384,7 +420,6 @@ def validate_action_road(action):
         print("An error happened due to invalid data, please try again..\n")
 
 
-
 def action_handler_road(action):
     """
     function that will handle all the inputs of the user
@@ -395,8 +430,6 @@ def action_handler_road(action):
         the_town()
     elif action == 'mountain':
         the_mountain()
-
-
 
 
 print("Welcome to the Bald Mountain RPG")
